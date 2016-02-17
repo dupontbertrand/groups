276b75a3cbe89d4b9378942cc70e81368af7e5e5
dupontbertrand <dupontbertrand59@gmail.com>
2016-02-17 16:50:49 +0100

    add best in place

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'groups#index'
-  get 'randommmm' => 'groups#random', as: :tata 
+  get 'randommmm' => 'groups#random', as: :dunno
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
