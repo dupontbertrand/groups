59d788cec598db1c31cc1b6bc8ae35f5cf616d17
dupontbertrand <dupontbertrand59@gmail.com>
2016-02-18 10:02:23 +0100

    add sensei in first table

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,12 @@
 class Group < ActiveRecord::Base
   has_many :people
+
+
+  def maitre
+    if self.people != nil && self.people.where(sensei: true).first != nil
+      self.people.where(sensei: true).first.name
+    else
+      ""
+    end
+  end
 end
